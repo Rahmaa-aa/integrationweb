9fce5f03ce0c5f021fdcd2c7bebd49753c908a95
Ala Ben Daoud <alabendawed@gmail.com>
2025-12-15 21:37:18 +0100

    Initial commit - travail de ala

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./discusion-publique.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        sidebar: "#0f172a",
+        sidebarLight: "#111827",
+        accent: "#f97316",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+
